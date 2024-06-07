efb4f6e7e2b6e76efa5d49047257910f10eaf033
SNEHAIP <snehaprasheed12@gmail.com>
2024-06-07 23:05:11 +0530

    finished

---
+++ src/components/AddAlbum.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const AddAlbum = () => {
+  return (
+    <div>
+        <NavBar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="form-label">Albumid</label>
+                            <input type="text" className="form-label" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="form-label">Name</label>
+                            <input type="text" className="form-label" />
+
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="form-label">Date</label>
+                        <input type="date" name="" id="" className="form-control" />
+                        
+
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="form-label">category</label>
+                        <input type="text" className="form-label" />
+
+
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="form-label">Feedback</label>
+                        <textarea name="" id="" className="form-control"></textarea>
+
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddAlbum
